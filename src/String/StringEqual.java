74fd480e1085eb46cd568a63f98458fcf5275cc5
MiMi <574609410@qq.com>
2017-05-01 13:00:08 +0800

    FD

--- src/String/StringEqual.java
+++ src/String/StringEqual.java
@@ -4,4 +4,14 @@ package String;
  * Created by MiMi on 2017/5/1.
  */
 public class StringEqual {
+    public static void main(String args[]){
+        String s1="hello";
+        String s2="hello";
+        String s3=new String("hi");
+        String s4=new String("hi");
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(s3==s4);
+        System.out.println(s3.equals(s4));
+    }
 }
